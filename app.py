749461d7978cdde4ffc09383ed402a3a01369279
Teoh TeikToe <teohteiktoe@gmail.com>
2025-11-14 12:08:15 +0000

    1

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 if os.path.exists('.env'):
     load_dotenv()
 
-os.environ["GROQ_API_KEY"] = os.environ.get('GROQ_API_KEY')
+# for AWS, do not run this because not using .env
+# os.environ["GROQ_API_KEY"] = os.environ.get('GROQ_API_KEY')
 
 client = Groq()
 
